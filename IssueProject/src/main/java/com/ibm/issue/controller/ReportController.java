36c19209eb23afb9dbbe5ce78eb0a768c77df5c7
kinchio37 <kinchio@163.com>
2020-11-18 12:11:26 +0800

    0.0

--- IssueProject/src/main/java/com/ibm/issue/controller/ReportController.java
+++ IssueProject/src/main/java/com/ibm/issue/controller/ReportController.java
@@ -1,6 +1,7 @@
 package com.ibm.issue.controller;
 
 import java.sql.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,14 +17,31 @@ import com.ibm.issue.service.ReportService;
 public class ReportController {
 	@Autowired
 	private ReportService reportService;
-	
+
+	/**
+	 * 创建issue表接口
+	 * @param issue
+	 * @return
+	 */
 	@PostMapping("createReport")
 	public Integer create(@RequestBody ReportWithBLOBs issue ) {
 		return reportService.createReport(issue);	
 	}
 	
-	
+	/**
+	 * 查看issue報表接口
+	 * @param issue
+	 * @return
+	 */
+	@PostMapping("reportDetails")
+	public List<ReportWithBLOBs> reportDetails(@RequestBody ReportWithBLOBs issue) {
+		return reportService.details(issue);
+	}
 	
 
+	@PostMapping("solve")
+	public ReportWithBLOBs solution(@RequestBody ReportWithBLOBs issue) {
+		return reportService.solve(issue);
+	}
 	
 }
